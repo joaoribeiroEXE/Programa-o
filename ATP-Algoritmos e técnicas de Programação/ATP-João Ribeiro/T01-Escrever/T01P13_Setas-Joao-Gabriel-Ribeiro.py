61cf3c284e2de1a166ae391dd96229e9c8210376
joaoribeiroEXE <joao.ribeiro3@aluno.ifsp.edu.br>
2025-09-10 22:37:12 -0300

    Add files via upload

---
+++ ATP-Algoritmos e técnicas de Programação/ATP-João Ribeiro/T01-Escrever/T01P13_Setas-Joao-Gabriel-Ribeiro.py
@@ -0,0 +1,13 @@
+#NomeProg..: [T01P13_Setas-Joao-Gabriel-Ribeiro.py]
+#Descrição...: xx) Os desenhos, com asteriscos ou outro caracter qualquer, de uma (1) seta para cima,
+# uma (1) para baixo, uma (1) para esquerda e uma (1) para direita. [T01P13_Setas-Joao-Gabriel-Ribeiro.py]
+#Autor...........: João Gabriel Ribeiro
+
+
+print("  *  \n *** \n*****\n  *  \n  *  \n  *  ")
+
+print("  *  \n  *  \n  *  \n*****\n *** \n  *  ")
+
+print("  *  \n **  \n*****\n **  \n  *  ")
+
+print("  *  \n  ** \n*****\n  ** \n  *  ")
