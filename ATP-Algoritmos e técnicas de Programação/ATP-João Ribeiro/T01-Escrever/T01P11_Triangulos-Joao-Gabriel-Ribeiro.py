160e3f980683bb5e6f95cee04eb73814cc231eec
joaoribeiroEXE <joao.ribeiro3@aluno.ifsp.edu.br>
2025-09-10 22:15:39 -0300

    Add files via upload

---
+++ ATP-Algoritmos e técnicas de Programação/ATP-João Ribeiro/T01-Escrever/T01P11_Triangulos-Joao-Gabriel-Ribeiro.py
@@ -0,0 +1,14 @@
+#NomeProg..: [T01P11_Triangulos-Joao-Gabriel-Ribeiro.py]
+#Descrição...: xx) Os desenhos, com asteriscos ou outro caracter qualquer, de um (1) triângulo
+#retângulo, um (1) triângulo equilátero, um (1) triângulo isósceles e um (1) triângulo
+#escaleno. [T01P11_Triangulos-Joao-Gabriel-Ribeiro.py]
+#Autor...........: Joao Gabriel Ribeiro  
+
+print("*\n**\n* *\n*  *\n*****")
+
+print("  *  \n * * \n*   *\n*****")
+
+print("  *  \n * * \n*   *\n*   *\n*****")
+
+print("*\n**\n* *\n*  *\n*****")
+
