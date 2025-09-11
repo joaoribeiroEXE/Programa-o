d209bd197d96d76d3995171a6c9cdc60fb966f37
joaoribeiroEXE <joao.ribeiro3@aluno.ifsp.edu.br>
2025-09-10 22:42:01 -0300

    Add files via upload

---
+++ ATP-Algoritmos e técnicas de Programação/ATP-João Ribeiro/T01-Escrever/T01P15_NomeProgramador-Joao-Gabriel-Ribeiro.py
@@ -0,0 +1,12 @@
+#NomeProg..: [T01P15_NomeProgramador-Joao Gabriel Ribeiro.py]
+#Descrição...: xx) Os desenhos, com asteriscos ou outro caracter qualquer, das letras que formam o seu
+#nome. [T01P15_NomeProgramador-Joao Gabriel Ribeiro.py]
+#Autor...........: Joao Gabriel Ribeiro
+
+print("*****\n*    \n**** \n*    \n*    \n")       
+print("*****\n*    \n**** \n*   \n*****\n")      
+print("*    \n*    \n*    \n*    \n*****\n") 
+print("*   *\n*   *\n * * \n  *  \n  *  \n")             
+print("**** \n*   *\n**** \n*   \n* \n")       
+print("*****\n*    \n**** \n*   \n*****\n") 
+input()      
