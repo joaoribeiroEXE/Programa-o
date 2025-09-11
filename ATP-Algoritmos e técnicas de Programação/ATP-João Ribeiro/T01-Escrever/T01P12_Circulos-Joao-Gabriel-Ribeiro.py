709e49042d9edaaecdeee3baf01b456069538694
joaoribeiroEXE <joao.ribeiro3@aluno.ifsp.edu.br>
2025-09-10 22:35:04 -0300

    Add files via upload

---
+++ ATP-Algoritmos e técnicas de Programação/ATP-João Ribeiro/T01-Escrever/T01P12_Circulos-Joao-Gabriel-Ribeiro.py
@@ -0,0 +1,11 @@
+#NomeProg..: [T01P12_Circulos-Joao-Gabriel-Ribeiro.py]
+#Descrição...: xx) Os desenhos, com asteriscos ou outro caracter qualquer, de um (1) círculo, uma (1)
+#elipse com comprimento vertical e uma (1) elipse com comprimento horizontal. [T01P12_Circulos-Nome.py]
+#Autor...........: Felype Joao Gabriel Ribeiro
+
+print("  ***  \n *   * \n*     *\n*     *\n *   * \n  ***  ")
+
+print("  ***  \n *   * \n*     *\n*     *\n*     *\n *   * \n  ***  ")
+
+print("   *****   \n *       * \n*         *\n*         *\n *       * \n   *****   ")
+input()
