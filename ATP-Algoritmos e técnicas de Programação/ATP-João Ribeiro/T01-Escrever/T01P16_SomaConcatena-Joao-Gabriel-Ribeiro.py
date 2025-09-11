7ee2e757ab6ca63b5fd67e44af7214017d2a60d8
joaoribeiroEXE <joao.ribeiro3@aluno.ifsp.edu.br>
2025-09-10 22:44:20 -0300

    Add files via upload

---
+++ ATP-Algoritmos e técnicas de Programação/ATP-João Ribeiro/T01-Escrever/T01P16_SomaConcatena-Joao-Gabriel-Ribeiro.py
@@ -0,0 +1,10 @@
+#NomeProg..: [T01P16_SomaConcatena-Joao Gabriel Ribeiro.py]
+#Descrição...: xx) Os três (3) tipos de soma/concatenação: [T01P16_NomeProgramador-Joao Gabriel Ribeiro.py]
+#Autor...........: Joao Gabriel Ribeiro
+print("1 + 1") 
+print("1" + "1") 
+print(1 + 1)
+input()
+
+
+
---
+++ ATP-Algoritmos e técnicas de Programação/ATP-João Ribeiro/T01-Escrever/T01P17_Calc7Oper-Joao-Gabriel-Ribeiro.py
@@ -0,0 +1,46 @@
+#NomeProg..: [T01P17_SomaConcatena-Joao Gabriel Ribeiro.py]
+#Descrição...: xx) O cálculo das 8 operações matemáticas: adição (+), subtração (-), multiplicação (*),
+#divisão de reais (/), divisão de inteiros (//), resto da divisão de inteiros (%),
+#exponenciação ou potenciação de reais (**) e radiciação ou raiz de reais (**(1/i)). [T01P17_NomeProgramador-Joao Gabriel Ribeiro.py]
+#Autor...........: Joao Gabriel Ribeiro
+
+num1=5
+num2=3
+resultado=num1+num2
+print(resultado)
+
+num3=4
+num4=2
+resultado=num3-num4
+print(resultado)
+
+num5=5
+num6=5
+resultado=num5*num6
+print(resultado)
+
+num7=10
+num8=5
+resultado=num7/num8
+print(resultado)
+
+num9=10
+numa=10
+resultado=num9//numa
+print(resultado)
+
+numb=1000
+numc=10
+resultado=numb%numc
+print(resultado)
+
+numd=100
+nume=10
+resultado=numd**nume
+print(resultado)
+
+numf=25
+numg=10
+resultado=numf**(1/numg)
+print(resultado)
+input()
